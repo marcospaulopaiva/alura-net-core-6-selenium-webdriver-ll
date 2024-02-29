56aa74b074cd73ba91c886bbd4ceb697db3a6a7d
Marcos Paulo <marcos.poo@gmail.com>
2024-02-28 22:35:30 -0300

    Aula 1 - Elementos invisíveis
    Nessa aula, aprendemos:
    - Nessa aula aprendemos a usar as Interactions do Selenium para simular ações do usuário que vão permitir lidar com elementos que só irão aparecer depois de determinada ação.

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class DashboardInteressadaPO
+    {
+        private IWebDriver driver;
+        private By byLogoutLink;
+        private By byMeuPerfilLink;
+
+        public DashboardInteressadaPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            byLogoutLink = By.Id("logout");
+            byMeuPerfilLink = By.Id("meu-perfil");
+        }
+
+        internal void EfetuarLogout()
+        {
+            var linkMeuPerfil = driver.FindElement(byMeuPerfilLink);
+            var linkLogout = driver.FindElement(byLogoutLink);
+
+            IAction acaoLogout = new Actions(driver)
+                //mover para o elemento meu-perfil
+                .MoveToElement(linkMeuPerfil)
+                //mover para o link de logout
+                .MoveToElement(linkLogout)
+                //clicar no link de logout
+                .Click()
+                .Build();
+
+            acaoLogout.Perform();
+
+        }
+    }
+}
---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarLogout.cs
@@ -0,0 +1,36 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarLogout
+    {
+        private IWebDriver _driver;
+
+        public AoEfetuarLogout(TestFixture fixture)
+        {
+            _driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginValidoDeveIrParaHomeNaoLogada()
+        {
+            //Arrange
+            var loginPO = new LoginPO(_driver);
+            loginPO.Visitar();
+            loginPO.PreencheFormulario("marcos.poo@gmail.com", "123");
+            loginPO.SubmeteFormulario();
+
+            var dashboardPO = new DashboardInteressadaPO(_driver);
+
+            //Act - efetuar logout
+            dashboardPO.EfetuarLogout();
+
+            //Assert
+            Assert.Contains("Próximos Leilões", _driver.PageSource);
+        }
+    }
+}
