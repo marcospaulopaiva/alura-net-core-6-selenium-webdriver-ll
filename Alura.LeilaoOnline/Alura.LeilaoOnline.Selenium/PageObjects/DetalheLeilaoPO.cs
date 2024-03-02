537502765fe70f20acf597ca2b09e2f243dc49fb
Marcos Paulo <marcos.poo@gmail.com>
2024-03-02 19:37:20 -0300

    Aula 4 - Esperando para as verificações.
    Nessa aula você:
    - conheceu o recurso do Selenium chamado Wait Commands, que permite esperar um determinado intervalo de tempo antes de verificar nossas expectativas de teste.
    - aprendeu que o Selenium WebDriver possui vários tipos de tempos de espera definidos. Por exemplo:
    -- O ImplicitWait é usado pelos métodos FindElement e FindElements para definir o timeout antes de lançar a exceção NoSuchElementException.
    -- O PageLoad define o timeout do carregamento de uma página quando a propriedade Url é setada ou através do método GoToUrl()
    -- AsynchronousJavaScript define o timeout quando tentamos executar um javascript assíncrono.
    -- Além desses podemos definir nossos próprios tempos para cenários específicos usando a classe WebDriverWait

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/PageObjects/DetalheLeilaoPO.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class DetalheLeilaoPO
+    {
+        private IWebDriver driver;
+        private By byInputValor;
+        private By byBotaoOfertar;
+        private By byLanceAtual;
+
+        public double LanceAtual
+        {
+            get
+            {
+                var valorTexto = driver.FindElement(byLanceAtual).Text;
+                var valor = double.Parse(valorTexto, System.Globalization.NumberStyles.Currency);
+                return valor;
+            }
+        }
+
+        public DetalheLeilaoPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            byInputValor = By.Id("Valor");
+            byBotaoOfertar = By.Id("btnDarLance");
+            byLanceAtual = By.Id("lanceAtual");
+        }
+
+        public void Visitar(int idLeitao)
+        {
+            driver.Navigate().GoToUrl($"http://localhost:5000/Home/Detalhes/{idLeitao}");
+        }
+
+        public void OfertarLance(double valor)
+        {
+            driver.FindElement(byInputValor).SendKeys(valor.ToString());
+            driver.FindElement(byBotaoOfertar).Click();
+        }
+    }
+}
