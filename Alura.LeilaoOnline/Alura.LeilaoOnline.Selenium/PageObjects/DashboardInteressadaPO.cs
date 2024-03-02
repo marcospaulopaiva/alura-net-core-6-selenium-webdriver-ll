48a13553e4700112150c557194d09af9035dbaaf
Marcos Paulo <marcos.poo@gmail.com>
2024-03-02 17:41:43 -0300

    Exemplo de interação com um dropdownlist.

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
@@ -1,10 +1,5 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Alura.LeilaoOnline.Selenium.PageObjects
 {
@@ -13,12 +8,64 @@ namespace Alura.LeilaoOnline.Selenium.PageObjects
         private IWebDriver driver;
         private By byLogoutLink;
         private By byMeuPerfilLink;
+        private By bySelectCategorias;
+        private By byInputTermo;
+        private By byInputAndamento;
+        private By byBotaoPesquisar;
 
         public DashboardInteressadaPO(IWebDriver driver)
         {
             this.driver = driver;
             byLogoutLink = By.Id("logout");
             byMeuPerfilLink = By.Id("meu-perfil");
+
+            bySelectCategorias = By.ClassName("select-wrapper");
+            byInputTermo = By.Id("");
+            byInputAndamento = By.Id("");
+            byBotaoPesquisar = By.Id("");
+        }
+
+        public void PesquisarLeiloes(List<string> categorias) 
+        {
+            //Seleciona e abre o dropdownlist de categorias.
+            var selectWrapper = driver.FindElement(bySelectCategorias);
+            selectWrapper.Click();
+
+            //Pausa de 2 segundos para ver a execução da ação de seleção.
+            Thread.Sleep(2000);
+
+            //Pega a lista de opções.
+            var opcoes = selectWrapper.FindElements(By.CssSelector("li>span")).ToList();
+
+            //Desmarcando as opções.
+            opcoes.ForEach(o =>
+            {
+                o.Click();
+            });
+
+            //Pausa de 2 segundos para ver a execução da ação de seleção.
+            Thread.Sleep(2000);
+
+            //Seleciona as opções baseado na lista passsada. 
+            categorias.ForEach(categ =>
+            {
+                opcoes
+                    .Where(o => o.Text.Contains(categ))
+                    .ToList()
+                    .ForEach(o =>
+                    {
+                        o.Click();
+                    });
+            });
+
+            //Tira o foco do dropdownlist com um Tab.
+            selectWrapper
+                .FindElement(By.TagName("li"))
+                .SendKeys(Keys.Tab);
+
+            //Pausa de 2 segundos para ver a execução da ação de seleção.
+            Thread.Sleep(2000);
+
         }
 
         internal void EfetuarLogout()
---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/Testes/AoFiltrarLeiloes.cs
@@ -0,0 +1,41 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoFiltrarLeiloes
+    {
+        private IWebDriver _driver;
+
+        public AoFiltrarLeiloes(TestFixture fixture)
+        {
+            _driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginInteressadaDeveMostraPainelResultado()
+        {
+            //Arrange
+            var loginPO = new LoginPO(_driver);
+            loginPO.Visitar();
+            loginPO.PreencheFormulario("marcos.poo@gmail.com", "123");
+            loginPO.SubmeteFormulario();
+
+            var dashboardInteressadaPO = new DashboardInteressadaPO(_driver);
+
+            //Act
+            dashboardInteressadaPO.PesquisarLeiloes(new List<string> { "Arte", "Coleções" });
+
+            //Assert
+
+        }
+    }
+}
